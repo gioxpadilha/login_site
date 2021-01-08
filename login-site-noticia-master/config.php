09edac642b25a6d67a025502bf1587aa5af276b5
gioxpadilha <57300930+gioxpadilha@users.noreply.github.com>
2021-01-08 17:09:02 -0300

    Add files via upload

--- login-site-noticia-master/config.php
+++ login-site-noticia-master/config.php
@@ -1,13 +1,13 @@
 <?php
 	session_start();
-	define("HOME_URI", "http://giovanapadilha-com.umbler.net///");
+	define("HOME_URI", "http://giovanaloginnoticias-com.umbler.net///");
 	define("HOME_DIR", "");
 
 	/*Parâmetros de conexão com o banco de dados*/
 	define('SGBD','mysql');
-	define('HOST_DB', 'mysql742.umbler.com');
-	define('DB', 'usuariogiovanas');
-	define('USER_DB', 'giovanapadilha');
+	define('HOST_DB', 'mysq380.umbler.com');
+	define('DB', 'usuariogiovanass');
+	define('USER_DB', 'giovanapadilhaa');
 	define('PASS_DB', 'testehospedagem');
 
 	define("DEFAULT_PASS", "usuario");
